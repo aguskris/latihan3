30d9b74ed4dde922719ed9b0fed0c0de5562cd87
aguskris <agusabad2000@gmail.com>
2018-04-08 12:07:58 +0700

    memperhatikan class aksi

--- sepedamotorAksi.java
+++ sepedamotorAksi.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package latihan3;
 
-/**
- *
- * @author user
- */
 public class sepedamotorAksi {
-    
+     public static void main(String[] args){
+        sepedamotor I = new sepedamotor();    
 }
