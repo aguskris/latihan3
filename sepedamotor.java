a6d22cd573e1d0441ab071d813951219f25228ea
aguskris <agusabad2000@gmail.com>
2018-04-08 11:51:58 +0700

    memasukan atribut methot

--- sepedamotor.java
+++ sepedamotor.java
@@ -10,4 +10,30 @@ public class sepedamotor {
                            "Warna \t:   "+warna+"\n"+
                            "Harga  \t:"+harga);
    }
-   }
+   
+   
+    public String getmerk(){
+      return merk;
+    }  
+
+    public void setMerk(String merk){
+          this.merk = merk;
+    }
+
+    public String getWarna(){
+          return warna;
+    }  
+
+    public void setWarna(String warna){
+          this.warna = warna;
+    }
+
+    public Double getHarga(){
+          return harga;
+    }  
+
+    public void setHarga(double harga){
+          this.harga = harga;
+     }
+   
+}
