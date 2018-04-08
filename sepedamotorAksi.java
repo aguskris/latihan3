d53dcdc6fb398dc76f08cbaa969f164fd8da2540
aguskris <agusabad2000@gmail.com>
2018-04-08 12:12:49 +0700

    isi atribut dan panggil cetak info

--- sepedamotorAksi.java
+++ sepedamotorAksi.java
@@ -8,4 +8,13 @@ public class sepedamotorAksi {
         I.setWarna("BIru");
         I.setHarga(14000000);
         
+        I.cetakInfo();
+        
+        System.out.print("Merknya \t: ");
+        System.out.println(I.getmerk());
+        System.out.print("Warnanya \t: ");
+        System.out.println(I.getWarna());
+        System.out.print("Harganya \t: ");
+        System.out.println(I.getHarga());
+        
      }
