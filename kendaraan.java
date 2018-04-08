31845e38240018480dd0f43e7da4050da5a6d056
aguskris <agusabad2000@gmail.com>
2018-04-08 12:17:09 +0700

    buat class extends sepeda motor dan atribut private jenis kendaraan

--- kendaraan.java
+++ kendaraan.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package latihan3;
 
-/**
- *
- * @author user
- */
 public class kendaraan {
-    
+    private String jenisKendaraan;
+
+    void throttle(){
+        System.out.println("Yamaha indonesia");
 }
