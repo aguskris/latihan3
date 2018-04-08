0138d202a0597bb042d31badf03d55fa40249df7
aguskris <agusabad2000@gmail.com>
2018-04-08 12:10:57 +0700

    memasukan atribut aksi

--- sepedamotorAksi.java
+++ sepedamotorAksi.java
@@ -3,4 +3,9 @@ package latihan3;
 public class sepedamotorAksi {
      public static void main(String[] args){
         sepedamotor I = new sepedamotor();    
-}
+           
+        I.setMerk("juviter");
+        I.setWarna("BIru");
+        I.setHarga(14000000);
+        
+     }
