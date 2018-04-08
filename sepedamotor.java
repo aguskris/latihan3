1be200fcf024b25a0cc852c955b52d369a13bda3
aguskris <agusabad2000@gmail.com>
2018-04-08 11:46:21 +0700

    memasukan atribut

--- sepedamotor.java
+++ sepedamotor.java
@@ -1,7 +1,13 @@
 package latihan3;
 
 public class sepedamotor {
-    String merk;
+   String merk;
    String warna;
    double harga;
-}
+   
+   void cetakInfo(){
+        System.out.println("Merk \t:    "+merk+"\n"+
+                           "Warna \t:   "+warna+"\n"+
+                           "Harga  \t:"+harga);
+   }
+   }
